49373e78077da718da6a410f8b6c9e1b9a4d6cc8
Ilya Dorofeev <dorofeev.ilya@gmail.com>
2018-01-29 23:52:38 +0300

    Selecting slick profile by jdbc driver from config

--- pubsub/src/main/scala/pubsub/BrokerDatabase.scala
+++ pubsub/src/main/scala/pubsub/BrokerDatabase.scala
@@ -1,8 +1,20 @@
 package pubsub
 
-object BrokerDatabase extends BrokerDatabaseSchema {
-  override val profile = slick.jdbc.H2Profile
+import com.typesafe.config.ConfigFactory
 
-  import profile.api._
-  lazy val db: Database = Database.forConfig("pubsub.broker.db")
+object BrokerDatabase extends BrokerDatabaseSchema {
+
+  private val config = ConfigFactory.load()
+  private val dbConfig = config.getConfig("pubsub.broker.db")
+  private val driver = dbConfig.getString("driver")
+
+  override val profile = {
+    driver match {
+      case "org.h2.Driver" => slick.jdbc.H2Profile
+    }
+  }
+
+  import profile.api.Database
+
+  override lazy val db: Database = Database.forConfig("", dbConfig)
 }
