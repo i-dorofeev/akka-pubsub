e5ff8c2ef02676e0d9d2dd01c525bcd29e477ad9
Ilya Dorofeev <dorofeev.ilya@gmail.com>
2018-03-29 22:24:39 +0300

    Refactored fixtures for SubscriptionActorStateTests

--- pubsub/src/test/scala/pubsub/SubscriptionActorStateTests.scala
+++ pubsub/src/test/scala/pubsub/SubscriptionActorStateTests.scala
@@ -17,42 +17,58 @@ class SubscriptionActorStateTests extends BaseTestKit("SubscriptionActorTest", A
     .withGuardianSupervisorStrategy[StoppingSupervisorStrategy])
   with MockFactory {
 
-  trait Fixture {
-    val waitDuration = 100 millis
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
 
-    // This is a receiver of event notifications
-    val subscriber = TestProbe("subscriber")
+  /** Stubs an event store.
+    *
+    * Provides a queue to manually emit event notifications in controlled fashion in test environment.
+    */
+  trait EventStoreStubFixture {
+    def topic: String
+
+    // Manual event store stub
+    val eventStoreStub: EventStore = stub[EventStore]
+
+    val eventSourceQueue = Source.queue[String](bufferSize = 1, OverflowStrategy.fail).preMaterialize() match {
+      case (queue, source) =>
+
+        // prepare manually controlled stub event stream for topic
+        (eventStoreStub.eventUpstream _).when(topic, *).onCall { (_, startFrom: EventOrdinal) =>
+          source
+            .zipWithIndex
+            .map { case (item, index) => EventNotification(ordinal = index + startFrom + 1, payload = item) }
+            .runWith(Sink.asPublisher(fanout = false)) }
+
+        queue
+    }
+  }
+
+  trait EndStateFixture {
 
     // End state receives the state value from the state under test and ends the state flow
-    val endState = stub[FSMActorState[EventOrdinal]]
-    (endState.onEnter _).when(*).onCall { i: EventOrdinal => Leave(i) }
-    (endState.name _).when().returning("endState")
+    val EndState = stub[FSMActorState[EventOrdinal]]
+    (EndState.onEnter _).when(*).onCall { i: EventOrdinal => Leave(i) }
+    (EndState.name _).when().returning("endState")
 
     /** Verifies the state value after leaving the state under test.
       *
       * @param expectedStateValue Expected state value.
       */
-    def verifyStateValue(expectedStateValue: EventOrdinal) = (endState.onEnter _).verify(expectedStateValue)
-
-    val topic = "testTopic"
-
-    // Manual event store stub
-    val eventStoreStub: EventStore = stub[EventStore]
-
-    implicit val materializer: ActorMaterializer = ActorMaterializer()
-    val (manualEventStream, source) = Source.queue[String](bufferSize = 1, OverflowStrategy.fail).preMaterialize()
-    (eventStoreStub.eventUpstream _).when(topic, *).onCall { (_, startFrom: EventOrdinal) =>
-      source
-        .zipWithIndex
-        .map { zip => EventNotification(ordinal = zip._2 + startFrom + 1, payload = zip._1) }
-        .runWith(Sink.asPublisher(fanout = false)) }
+    def verifyStateValue(expectedStateValue: EventOrdinal) = (EndState.onEnter _).verify(expectedStateValue)
   }
+
+  class Fixture(val topic: String) extends EventStoreStubFixture with EndStateFixture {
+    // This is a receiver of event notifications
+    val subscriber = TestProbe("subscriber")
+  }
+
+  val waitDuration = 100 millis
 
   "A SubscriptionActor" when {
     "created" should {
-      "send SubscriptionAck and leave" in new Fixture {
+      "send SubscriptionAck and leave" in new Fixture("testTopic") {
         val subscriptionActorProps = Props(new SubscriptionActor(subscriber.ref, topic, 1, eventStoreStub) {
-          override val stateFlow = Created >>: endState
+          override val stateFlow = Created >>: EndState
         })
 
         val actor = watch (system.actorOf(subscriptionActorProps))
@@ -67,18 +83,18 @@ class SubscriptionActorStateTests extends BaseTestKit("SubscriptionActorTest", A
     }
 
     "catching up with event upstream" should {
-      "accept events from upstream and forward them to the subscriber" in new Fixture {
+      "accept events from upstream and forward them to the subscriber" in new Fixture("testTopic") {
         val subscriptionActorProps = Props(new SubscriptionActor(subscriber.ref, topic, 1, eventStoreStub) {
-          override val stateFlow = CatchingUpWithUpstream >>: endState
+          override val stateFlow = CatchingUpWithUpstream >>: EndState
         })
 
         val actor = watch (system.actorOf(subscriptionActorProps))
 
-        manualEventStream.offer("event2")
-        manualEventStream.offer("event3")
+        eventSourceQueue.offer("event2")
+        eventSourceQueue.offer("event3")
         actor ! EventNotification(5, "event5") // we shouldn't accept event notifications from topic in this state
-        manualEventStream.offer("event4")
-        manualEventStream.complete()
+        eventSourceQueue.offer("event4")
+        eventSourceQueue.complete()
 
         subscriber.expectMsg(EventNotification(2L, "event2"))
         subscriber.expectMsg(EventNotification(3L, "event3"))
@@ -93,9 +109,9 @@ class SubscriptionActorStateTests extends BaseTestKit("SubscriptionActorTest", A
     }
 
     "waiting for event notifications" should {
-      "forward event notifications from topic to subscriber" in new Fixture {
+      "forward event notifications from topic to subscriber" in new Fixture("testTopic") {
         val subscriptionActorProps = Props(new SubscriptionActor(subscriber.ref, topic, 1, eventStoreStub) {
-          override val stateFlow = WaitingForEvents >>: endState
+          override val stateFlow = WaitingForEvents >>: EndState
         })
 
         val actor = watch (system.actorOf(subscriptionActorProps))
